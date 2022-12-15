39135e7ada1ba804eabbc310668e4a352473b672
harofax <daniel.kh555@gmail.com>
2022-12-15 10:43:42 +0100

    added constructors to comps

--- WavesOfHaraxis/ecs/components.h
+++ WavesOfHaraxis/ecs/components.h
@@ -23,43 +23,73 @@ namespace ecs
         static constexpr auto component_id = static_cast<std::size_t>(ID);
     };
 
-    struct SpriteComponent : Component<SpriteComponent, ComponentType::Sprite>
+    struct SpriteComponent : public Component<SpriteComponent, ComponentType::Sprite>
     {
+	    explicit SpriteComponent(int spr_id) : sprite_index(spr_id) {}
 	    int sprite_index = -1;
     };
 
-    struct BoundsComponent : Component<BoundsComponent, ComponentType::Bounds>
+    struct BoundsComponent : public Component<BoundsComponent, ComponentType::Bounds>
     {
+        BoundsComponent(int W, int H) : w(W), h(H) {}
         int w;
         int h;
     };
 
-    struct TransformComponent : Component<TransformComponent, ComponentType::Transform>
+    struct TransformComponent : public Component<TransformComponent, ComponentType::Transform>
     {
+	    TransformComponent(int x, int y)
+		    : x(x),
+		      y(y)
+	    {
+	    }
+
 	    int x;
         int y;
     };
 
-    struct VelocityComponent : Component<VelocityComponent, ComponentType::Velocity>
+    struct VelocityComponent : public Component<VelocityComponent, ComponentType::Velocity>
     {
+	    VelocityComponent(int x_vel, int y_vel)
+		    : x_vel(x_vel),
+		      y_vel(y_vel)
+	    {
+	    }
+
 	    int x_vel;
         int y_vel;
     };
 
-    struct HealthComponent : Component<HealthComponent, ComponentType::Health>
+    struct HealthComponent : public Component<HealthComponent, ComponentType::Health>
     {
+	    explicit HealthComponent(int health)
+		    : health(health)
+	    {
+	    }
+
 	    int health;
     };
 
-    struct WeaponComponent : Component<WeaponComponent, ComponentType::Weapon>
+    struct WeaponComponent : public Component<WeaponComponent, ComponentType::Weapon>
     {
+	    WeaponComponent(int damage, bool shoot_up)
+		    : damage(damage),
+		      shoot_up(shoot_up)
+	    {
+	    }
+
 	    int damage;
         // only shoot up or down, so made it a bool
         bool shoot_up;
     };
 
-    struct DamagingComponent : Component<DamagingComponent, ComponentType::Damaging>
+    struct DamagingComponent : public Component<DamagingComponent, ComponentType::Damaging>
     {
+	    explicit DamagingComponent(int damage)
+		    : damage(damage)
+	    {
+	    }
+
 	    int damage;
     };
 
